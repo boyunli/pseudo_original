0e5034ac92276c7f2382339b6cfdfe367ebb41b1
liling <liling@meifang.com>
2018-11-28 12:15:36 +0800

    change re rule

--- src/utils/comm.py
+++ src/utils/comm.py
@@ -3,4 +3,4 @@ import re
 SEPARATOR = '<br/><br/>'
 
 def trim(line):
-    return re.sub(r'(</p>|<br>|<p>|\d|\s|\r)', '', line)
+    return re.sub(r'(</p>|<br>|<p>|\s|\r|微信|VX|卫星|威信|微芯|公众号|工棕号|关注|添加|薇信|唯芯|\+V|vx)', '', line)
